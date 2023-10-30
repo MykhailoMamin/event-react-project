fced83b1324f0d769d9d4e959ad8e5c39397123a
MykhailoMamin <mishamamin2016@gmail.com>
2023-10-31 00:08:36 +0200

    second commit

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,13 +2,32 @@ import { Component } from 'react';
 import css from './Counter.module.css';
 
 class Counter extends Component {
+  state = {
+    value: 0,
+  };
+
+  handleIncrement = () => {
+    this.setState(prevState => ({
+      value: prevState.value + 1,
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState(prevState => ({
+      value: prevState.value - 1,
+    }));
+  };
   render() {
     return (
       <div>
-        <span>0</span>
+        <span>{this.state.value}</span>
         <div>
-          <button type="button">Збільшити на 1</button>
-          <button type="button">Зменшити на 1</button>
+          <button type="button" onClick={this.handleIncrement}>
+            Збільшити на 1
+          </button>
+          <button type="button" onClick={this.handleDecrement}>
+            Зменшити на 1
+          </button>
         </div>
       </div>
     );
